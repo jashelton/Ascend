779d575b16f8467ad1a2c540b2ed4dcee6b80bc7
jashelton <justin.shelton@yahoo.com>
2017-02-12 01:48:22 -0500

    adding list of projects for watching, progress, and completed

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  projects = [
+    {
+      id: 1,
+      name: 'Project Name 1',
+      address: '200 Waitlist Way, Garner NC 27529',
+      icon: 'home',
+      status: 'watching'
+    },
+    {
+      id: 2,
+      name: 'Project Name 2',
+      address: '201 Progress Way, Garner NC 27529',
+      icon: 'home',
+      status: 'in_progress'
+    },
+    {
+      id: 3,
+      name: 'Project Name 3',
+      address: '500 Completed Way, Garner NC 27529',
+      icon: 'home',
+      status: 'completed'
+    },
+  ]
 
   constructor() { }
 
