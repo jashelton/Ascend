4336dc5241d64af9a99a2d7621713f302b1f71fa
jashelton <justin.shelton@yahoo.com>
2017-02-12 02:14:42 -0500

    adding mocked home content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  types = ['Single Room', 'Full Remodel'];
+  typeSelected = false;
+  roomSelected = false;
+
+  rooms = ['Bedroom', 'Bathroom', 'Kitchen', 'Living Room'];
+  links = ['one', 'two', 'three', 'four'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  displaySettings() {
+    this.typeSelected = true;
+  }
+
+  displayMaterials() {
+    this.roomSelected = true;
   }
 
 }
